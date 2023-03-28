1be1e65d20228e900f709b14894b24065e3f1a05
Насим Наилевич <nasim-82@mail.ru>
2023-03-28 23:16:58 +0500

    enable logger

--- index.py
+++ index.py
@@ -5,8 +5,15 @@ from telegram import Update, ParseMode
 
 import os
 import re
+import logging
 
 from dotenv import load_dotenv
+
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 load_dotenv()  # loads the configs from .env
 
